4fe41ba80bb33f0f23e9e270f0ccb04b68eafe76
Ali <alimansour97@gmail.com>
2018-08-02 14:45:02 -0600

    add authform

--- vogelkop-client/src/containers/Main.js
+++ vogelkop-client/src/containers/Main.js
@@ -2,6 +2,7 @@ import React from "react";
 import {Switch, Route, withRouter, Redirect} from "react-router-dom"
 import {connect} from "react-redux"
 import Homepage from"../components/Homepage"
+import AuthForm from "../components/AuthForm"
 
 const Main = props=>{
     const {authUser}=props;
@@ -9,6 +10,16 @@ const Main = props=>{
         <div className="container">
             <Switch>
                 <Route exact path="/" render={props=> <Homepage {...props} />} />
+                <Route exact path="/signin" render={props=>{
+                return(
+                        <AuthForm buttonText="Log in" heading="Welcome back"{...props}/>
+                    )
+            }}/>
+      <Route exact path="/signup" render={props=>{
+                return(
+                        <AuthForm signUp buttonText="Sign up" heading="Join Vogelkop today"{...props}/>
+                    )
+            }}/>
             </Switch>
         </div>
     
